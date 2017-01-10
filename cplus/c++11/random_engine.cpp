4e77f830d4b7bfe11f1150fdf302cfe4579ef449
mashago <mashacoder@foxmail.com>
2017-01-10 08:23:26 +0800

    add cpp random

---
+++ cplus/c++11/random_engine.cpp
@@ -0,0 +1,113 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+
+#include <random>
+
+enum { RANDOM_TIMES = 5 };
+enum { RANDOM_RANGE = 5 };
+
+int test0()
+{
+	srand(time(NULL));
+	for (int i = 0; i < RANDOM_TIMES; i++)
+	{
+		int r = rand() % RANDOM_RANGE; // [0, RANDOM_RANGE)
+		printf("r=%d\n", r);
+	}
+
+	return 0;
+}
+
+int test1()
+{
+	std::default_random_engine e(time(NULL));
+	std::uniform_int_distribution<unsigned> u(0, RANDOM_RANGE); // [0, RANDOM_RANGE]
+	for (int i = 0; i < RANDOM_TIMES; i++)
+	{
+		int r = u(e); // [0, RANDOM_RANGE]
+		printf("r=%d\n", r);
+	}
+	return 0;
+}
+
+int test2()
+{
+	std::default_random_engine e(time(NULL));
+	std::uniform_real_distribution<double> u(0, 1); // [0, 1]
+	for (int i = 0; i < RANDOM_TIMES; i++)
+	{
+		double r = u(e); // [0, RANDOM_RANGE]
+		printf("r=%lf\n", r);
+	}
+	return 0;
+}
+
+int test3()
+{
+	std::default_random_engine e(time(NULL));
+	std::bernoulli_distribution u; // default 50/50
+	for (int i = 0; i < RANDOM_TIMES; i++)
+	{
+		bool flag = u(e); // 50/50
+		printf("flag=%s\n", flag?"true":"false");
+	}
+	return 0;
+}
+
+int test_notyet() 
+{
+	// int ret;
+
+	// ret = 0;
+	return 0;
+}
+
+typedef int (*testcase_t) ();
+testcase_t test_list[] =
+{
+	test0
+,	test1
+,	test2
+,	test3
+};
+
+int main(int argc, char *argv[]) 
+{
+	int ret;
+	int maxcase;
+	int testcase;
+
+	maxcase = sizeof(test_list) / sizeof(testcase_t);
+	testcase = maxcase - 1;
+
+	if (argc > 1) {
+		if (!strcmp(argv[1], "all")) {
+			printf("run all case\n");
+			for (int i=0; i<maxcase; i++) {
+				printf("run case[%d]\n", i);
+				ret = test_list[i]();
+				if (ret != 0) {
+					printf("case[%d] ret=%d\n", i, ret);
+					return 0;
+				}
+			}
+			return 0;
+		}
+		testcase = atoi(argv[1]);
+		if (testcase < 0 || testcase >= maxcase) {
+			testcase = maxcase - 1;
+		}
+	}
+
+	printf("run case[%d]\n", testcase);
+	ret = test_list[testcase]();
+	if (ret != 0) {
+		printf("case[%d] ret=%d\n", testcase, ret);
+	}
+
+	return 0;
+}
+
