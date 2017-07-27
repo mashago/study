c1e636d8a9cedb42c862d533edb2ce627caf07d9
mashago <mashacoder@foxmail.com>
2017-07-28 07:01:43 +0800

    add cmake for lua

---
+++ lua/clua/clean_cmake.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "rm cmake cache begin..."
+#rm -rf `find . -name "cmake_install.cmake"`
+#rm -rf `find . -name "CMakeFiles"`
+#rm -rf `find . -name "Makefile"`
+#rm -rf `find . -name "CMakeCache.txt"`
+rm -rf cmake_install.cmake
+rm -rf CMakeFiles
+rm -rf Makefile
+rm -rf CMakeCache.txt
+echo "rm cmake cache end..."
