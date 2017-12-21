93fb2867fce5a4260666c85aa0d1ae891054a3e3
mashago <mashacoder@foxmail.com>
2017-12-21 20:02:52 +0800

    update golang

--- golang/test1/src/main.go
+++ golang/test1/src/main.go
@@ -1,7 +1,23 @@
 package main
 
-import ("fmt")
+import (
+	"fmt"
+	"runtime"
+)
 
+// call init func second
+func init() {
+	fmt.Printf("in init\n")
+	buff = runtime.GOOS
+}
+
+// init var first
+var buff string
+
+// call main last
 func main() {
 	fmt.Printf("hello world\n")
+	fmt.Printf("buff=%s\n", buff)
+	n := 123
+	println("n=", n)
 }
