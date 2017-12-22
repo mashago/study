b93729df73791244de1f3ea2a0efc2402b5abad0
mashago <mashacoder@foxmail.com>
2017-12-22 19:47:13 +0800

    update go study

--- golang/test1/src/main.go
+++ golang/test1/src/main.go
@@ -12,12 +12,13 @@ func init() {
 }
 
 // init var first
-var buff string
+var buff string // only define, should define type
 
 // call main last
 func main() {
 	fmt.Printf("hello world\n")
 	fmt.Printf("buff=%s\n", buff)
-	n := 123
-	println("n=", n)
+	n1 := 111 // define and init
+	var n2 = 222 // define and init
+	println("n1=", n1, "n2=", n2)
 }
