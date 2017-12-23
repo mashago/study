47232eec745aa22f46b97d9098cc7e6eadd1aee5
mashago <mashacoder@foxmail.com>
2017-12-23 09:59:22 +0800

    fix go study

--- golang/test1/src/main.go
+++ golang/test1/src/main.go
@@ -8,54 +8,98 @@ import (
 	"strconv"
 )
 
-// call init func second
+// first, init var
+var buff string // only define, should define type
+
+// second, call init func
 func init() {
 	fmt.Printf("in init\n")
 	buff = runtime.GOOS
 }
 
+type testcast_f func()(int)
+
 func test0()(int) {
+
+	fmt.Printf("buff=%s\n", buff)
+	n1 := 111 // define and init
+	var n2 = 222 // define and init
+	println("n1=", n1, "n2=", n2)
+
 	return 0
 }
 
 func test1()(int) {
+	var b1 bool = true
+	var b2 bool = false
+	fmt.Printf("b1=%t b2=%t\n", b1, b2)
+
+	var i1 int = 111
+	var i2 int32 = 222
+	var i3 int64 = 333
+	fmt.Printf("i1=%d i2=%d i3=%d\n", i1, i2, i3)
+
+	var f1 float32 = 3.14
+	var f2 float64 = 3.1415926123456
+	fmt.Printf("f1=%f f1=%g f2=%f f2=%g\n", f1, f1, f2, f2)
+
 	return 0
 }
 
-type testcast_f func()(int)
-var test_list = []testcast_f{
-	test0
-,	test1
+func test2()(int) {
+	return 0
 }
 
-// init var first
-var buff string // only define, should define type
+func test3()(int) {
+	return 0
+}
 
-// call main last
+// is ok
+var test4 testcast_f = func()(int) {
+	return 0
+}
+
+// is ok
+var test5 func()(int) = func()(int) {
+	return 0
+}
+
+var test_list = []testcast_f{
+// var test_list = []func()(int){ -- is ok
+test0,
+test1,
+test2,
+test3,
+test4,
+test5,
+}
+
+// last, call main
 func main() {
 
 	fmt.Printf("hello golang\n")
-	fmt.Println(os.Args)
+	fmt.Printf("os.Args=%s\n", os.Args)
 
 	argc := len(os.Args)
 	argv := os.Args
 	maxcase := len(test_list)
 	testcase := maxcase - 1
+	var err error
 
 	if argc > 1 {
-		if !strings.Compare(argv[1], "all") {
-			fmt.Printf("run all case\n")
-			for i:= 1; i < maxcase; i++ {
+		if 0 == strings.Compare(argv[1], "all") {
+			for i:= 0; i < maxcase; i++ {
+				fmt.Printf("\nrun case[%d]\n", i)
 				ret := test_list[i]()
 				if ret != 0 {
 					fmt.Printf("cast[%d] ret=%d\n", i, ret)
-					return 0
+					return
 				}
 			}
-			return 0
+			return
 		}
-		testcase, err := strconv.Atoi(argv[1])
-		if testcase < 0 || testcase >= maxcase {
+		testcase, err = strconv.Atoi(argv[1])
+		if err != nil || testcase < 0 || testcase >= maxcase {
 			testcase = maxcase - 1
 		}
 	}
@@ -63,13 +107,6 @@ func main() {
 	fmt.Printf("run case[%d]\n", testcase)
 	ret := test_list[testcase]()
 	if ret != 0 {
-		fmt.Printf("cast[%d] ret=%d\n", i, ret)
+		fmt.Printf("cast[%d] ret=%d\n", testcase, ret)
 	}
-
-
-
-	fmt.Printf("buff=%s\n", buff)
-	n1 := 111 // define and init
-	var n2 = 222 // define and init
-	println("n1=", n1, "n2=", n2)
 }
