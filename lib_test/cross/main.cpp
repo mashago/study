33eae44a49c4f41f6604937b727b49389752527a
mashago <mashacoder@foxmail.com>
2019-09-21 07:50:21 +0800

    update loadlib for windows

--- lib_test/cross/main.cpp
+++ lib_test/cross/main.cpp
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <string.h>
 
-#ifdef _WIN32
+#ifdef WIN32
+#include <windows.h>
 #else
 #include <dlfcn.h>
 #endif
@@ -14,9 +15,9 @@ dl_func dl_load_func(void *lib, const char *sym);
 void dl_unload_lib(void *lib);
 char *dl_error(char *buffer, int size);
 
-#define USE_DLOPEN 1
+void pre_exit();
 
-#ifndef WIN23
+#ifndef WIN32
 
 void *dl_load_lib(const char *path)
 {
@@ -26,8 +27,8 @@ void *dl_load_lib(const char *path)
 
 dl_func dl_load_func(void *lib, const char *sym)
 {
-    dl_func open_func = (dl_func)dlsym(lib, sym);
-	return open_func;
+    dl_func func = (dl_func)dlsym(lib, sym);
+	return func;
 }
 
 void dl_unload_lib(void *lib)
@@ -42,22 +43,46 @@ char *dl_error(char *buffer, int size)
 	return buffer;
 }
 
+void pre_exit()
+{
+}
+
 #else
 
 void *dl_load_lib(const char *path)
 {
+	HMODULE lib = LoadLibraryExA(path, NULL, 0);
+	return lib;
 }
 
 dl_func dl_load_func(void *lib, const char *sym)
 {
+	dl_func func = (dl_func)GetProcAddress((HMODULE)lib, sym);
+	return func;
 }
 
 void dl_unload_lib(void *lib)
 {
+	FreeLibrary((HMODULE)lib);
 }
 
+#define snprintf(buffer, count, format, ...) do {_snprintf_s(buffer, count, count-1, format, ##__VA_ARGS__);} while (false)
 char *dl_error(char *buffer, int size)
 {
+	int error = GetLastError();
+	if (FormatMessageA(FORMAT_MESSAGE_IGNORE_INSERTS | FORMAT_MESSAGE_FROM_SYSTEM,
+		NULL, error, 0, buffer, size, NULL))
+	{
+		return buffer;
+	}
+
+	snprintf(buffer, size, "system error %d\n", error);
+	return buffer;
+}
+
+void pre_exit()
+{
+	getchar();
 }
 
 #endif
@@ -66,7 +91,7 @@ char *dl_error(char *buffer, int size)
 #if __APPLE__
 const char *libname = "lib/libmylib.dylib";
 #elif WIN32
-const char *libname = "lib/libmylib.dll";
+const char *libname = "../lib/mylib.dll";
 #else
 const char *libname = "lib/libmylib.so";
 #endif
@@ -81,15 +106,17 @@ int main(int argc, char** argv)
     void *lib = dl_load_lib(libname);
     if (!lib)
     {
-        printf("dlopen fail %s\n", dl_error(error_buffer, buffer_size));
+        printf("dl_load_lib fail %s\n", dl_error(error_buffer, buffer_size));
+		pre_exit();
         return 1;
     }
     printf("load lib success\n");
 
-    dl_func open_func = dl_load_func(lib, "myopen");
+    dl_func open_func = dl_load_func(lib, "myopen2");
     if (!open_func)
     {
-        printf("dlsym fail %s\n", dl_error(error_buffer, buffer_size));
+        printf("dl_load_func fail %s\n", dl_error(error_buffer, buffer_size));
+		pre_exit();
         return 1;
     }
 
@@ -97,6 +124,8 @@ int main(int argc, char** argv)
     printf("r1=%p\n", r1);
 
 	dl_unload_lib(lib);
-    
+
+	pre_exit();
+
     return 0;
 }
